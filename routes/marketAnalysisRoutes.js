95e9ceb78cba18d9ca44c46038544bb4b9ff3aa6
Sumanth <sumanth.talamanchi@passportinc.com>
2025-02-17 07:12:06 +0530

    file seperation

---
+++ routes/marketAnalysisRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const {
+  getAllMarketAnalysis,
+  getMarketAnalysis,
+  createMarketAnalysis,
+  updateMarketAnalysis,
+} = require("../controllers/marketAnalysisController");
+const router = express.Router();
+
+// app.post("/api/marketanalysis", createMarketAnalysis);
+// app.get("/api/marketanalysis", getAllMarketAnalysis);
+// app.patch("/api/marketanalysis/:id", updateMarketAnalysis);
+// app.get("/api/marketanalysis/:id", getMarketAnalysis);
+
+router.route("/").get(getAllMarketAnalysis).post(createMarketAnalysis);
+
+router.route("/:id").get(getMarketAnalysis).patch(updateMarketAnalysis);
+
+module.exports = router;
